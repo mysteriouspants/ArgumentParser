93dfa372c35bfa221b6a07b9f8a34701ae692855
Chris Miller <nserror@me.com>
2012-02-29 13:49:55 -0700

    More better example.

--- example/chocolat-app.m
+++ example/chocolat-app.m
@@ -22,29 +22,21 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        enum signatureIndexes {
-            async,
-            wait,
-            no_reativation,
-            help,
-            version
-        };
-        NSArray * signatures = [NSArray arrayWithObjects:
-                                [FSArgumentSignature argumentSignatureAsFlag:@"a" longNames:@"async"
-                                                             multipleAllowed:NO description:@"-a --async           Do not wait for the user to close the file in Chocolat. [default if output is ignored]"],
-                                [FSArgumentSignature argumentSignatureAsFlag:@"w" longNames:@"wait"
-                                                             multipleAllowed:NO description:@"-w --wait            Wait for file to be closed by Chocolat. [default if output is piped]"],
-                                [FSArgumentSignature argumentSignatureAsFlag:@"n" longNames:@"no-reactivation"
-                                                             multipleAllowed:NO description:@"-n --no-reactivation After editing with -w, do not reactivate the calling app."],
-                                [FSArgumentSignature argumentSignatureAsFlag:@"h" longNames:@"help"
-                                                             multipleAllowed:NO description:@"-h --help            Show this information."],
-                                [FSArgumentSignature argumentSignatureAsFlag:@"v" longNames:@"version"
-                                                             multipleAllowed:NO description:@"-v --version         Print version information."],
-                                nil];
+        FSArgumentSignature
+        * asyncSig = [FSArgumentSignature argumentSignatureAsFlag:@"a" longNames:@"async"
+                                                  multipleAllowed:NO description:@"-a --async           Do not wait for the user to close the file in Chocolat. [default if output is ignored]"],
+        * waitSig = [FSArgumentSignature argumentSignatureAsFlag:@"w" longNames:@"wait"
+                                                 multipleAllowed:NO description:@"-w --wait            Wait for file to be closed by Chocolat. [default if output is piped]"],
+        * noReactivationSig = [FSArgumentSignature argumentSignatureAsFlag:@"n" longNames:@"no-reactivation"
+                                                           multipleAllowed:NO description:@"-n --no-reactivation After editing with -w, do not reactivate the calling app."],
+        * helpSig = [FSArgumentSignature argumentSignatureAsFlag:@"h" longNames:@"help"
+                                                 multipleAllowed:NO description:@"-h --help            Show this information."],
+        * versionSig = [FSArgumentSignature argumentSignatureAsFlag:@"v" longNames:@"version"
+                                                    multipleAllowed:NO description:@"-v --version         Print version information."];
+        NSArray * signatures = [NSArray arrayWithObjects:asyncSig, waitSig, noReactivationSig, helpSig, versionSig, nil];
         NSError * err;
         FSArgumentPackage * arguments = [FSArgumentParser parseArguments:[[NSProcessInfo processInfo] arguments] withSignatures:signatures error:&err];
-        if (arguments && [[arguments.flags objectForKey:
-                           [signatures objectAtIndex:help]] boolValue]) {
+        if (arguments && [arguments boolValueOfFlag:helpSig]) {
             PrintLn(@"Usage: choc [-awdnhv] [file ...]");
             PrintLn(@"Options:");
             [signatures enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
@@ -54,7 +46,7 @@ int main(int argc, const char * argv[])
             PrintLn(@"By default choc will not wait for the file to be closed except when the output is not to the console:");
             PrintLn(@"ls *.tex|choc|sh	-w implied");
             PrintLn(@"choc foo.h > bar.h	-w implied\n");
-        } else if (arguments && [[arguments.flags objectForKey:[signatures objectAtIndex:version]] boolValue]) {
+        } else if (arguments && [arguments boolValueOfFlag:versionSig]) {
             PrintLn(@"Example app version nil");
         } else if (err) {
             // handle the error
@@ -71,9 +63,9 @@ int main(int argc, const char * argv[])
         } else {
             // handle the other args
             NSArray * filesToOpen = arguments.unnamedArguments;
-            BOOL doAsync = [[arguments.flags objectForKey:[signatures objectAtIndex:async]] boolValue];
-            BOOL doWait = [[arguments.flags objectForKey:[signatures objectAtIndex:wait]] boolValue];
-            BOOL doNotReactivate = [[arguments.flags objectForKey:[signatures objectAtIndex:no_reativation]] boolValue];
+            BOOL doAsync = [arguments boolValueOfFlag:asyncSig];
+            BOOL doWait = [arguments boolValueOfFlag:waitSig];
+            BOOL doNotReactivate = [arguments boolValueOfFlag:noReactivationSig];
             
             // do other things related to the app
         }
