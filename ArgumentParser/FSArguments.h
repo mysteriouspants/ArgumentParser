98a736e9e3d57e147e2fc79de1712d0ee54a926e
Chris Miller <nserror@me.com>
2012-04-02 10:26:05 -0600

    Added a convenience include header.

---
+++ ArgumentParser/FSArguments.h
@@ -0,0 +1,16 @@
+//
+//  FSArguments.h
+//  FSArgumentParser
+//
+//  Created by Christopher Miller on 4/2/12.
+//  Copyright (c) 2012 FSDEV. All rights reserved.
+//
+
+#ifndef IRCKit_FSArguments_h
+#define IRCKit_FSArguments_h
+
+#import "FSArgumentSignature.h"
+#import "FSArgumentParser.h"
+#import "FSArgumentPackage.h"
+
+#endif
